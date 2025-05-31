044609703c9f04229654e79820315508bb6eafdb
Nikita Podvirnyi <krypt0nn@vk.com>
2025-05-31 23:01:36 +0200

    chore: preparations for repo publishing

---
+++ README.md
@@ -0,0 +1,7 @@
+# Capacitor
+
+Logical continuation of my [markov-chains](https://github.com/krypt0nn/markov-chains)
+project for text generation using highly modified Markov chains architecture.
+
+Author: [Nikita Podvirnyi](https://github.com/krypt0nn)\
+Licensed under [GPL-3.0](LICENSE)
--- flake.nix
+++ flake.nix
@@ -18,7 +18,38 @@
                     overlays = [ rust-overlay.overlays.default ];
                 };
 
+                config = pkgs.lib.importTOML ./Cargo.toml;
+
             in {
+                packages.default = pkgs.rustPlatform.buildRustPackage {
+                    pname = config.package.name;
+                    version = config.package.version;
+
+                    src = ./.;
+                    cargoLock.lockFile = ./Cargo.lock;
+
+                    meta = with pkgs.lib; {
+                        description = config.package.description;
+                        homepage = config.package.homepage;
+                        license = licenses.gpl3Only;
+
+                        maintainers = [
+                            {
+                                name = "Nikita Podvirnyi";
+                                email = "krypt0nn@vk.com";
+                                matrix = "@krypt0nn:mozilla.org";
+                                github = "krypt0nn";
+                                githubId = 29639507;
+                            }
+                        ];
+                    };
+
+                    nativeBuildInputs = with pkgs; [
+                        rust-bin.stable.latest.minimal
+                        gcc
+                    ];
+                };
+
                 devShells.default = pkgs.mkShell {
                     nativeBuildInputs = with pkgs; [
                         (rust-bin.stable.latest.default.override {
