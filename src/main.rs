169de56cfb55e8388b06dfbf1310c4a75c005d31
Nikita Podvirnyi <krypt0nn@vk.com>
2025-05-31 16:33:36 +0200

    feat: implement model building command

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::str::FromStr;
 use std::path::PathBuf;
 
-use recipe::Recipe;
-
 pub mod tokens;
 pub mod tokenizer;
 pub mod transitions;
 pub mod clustering;
 pub mod recipe;
 pub mod model;
+
+use recipe::Recipe;
+use model::Model16;
 
 fn main() -> anyhow::Result<()> {
     let mut args = std::env::args().skip(1);
@@ -29,16 +30,34 @@ fn main() -> anyhow::Result<()> {
             let path = args.next()
                 .unwrap_or_else(|| String::from("capacitorfile"));
 
-            let path = PathBuf::from(path);
+            let mut path = PathBuf::from(path);
+
+            if path.is_dir() {
+                path = path.join("capacitorfile");
+            }
 
             if !path.is_file() {
                 anyhow::bail!("invalid recipe file path");
             }
 
             let recipe = std::fs::read_to_string(&path)?;
-            let recipe = Recipe::from_str(&recipe)?;
+            let mut recipe = Recipe::from_str(&recipe)?;
 
-            dbg!(recipe);
+            if let Some(parent) = path.parent() {
+                recipe = recipe.relative_to(parent);
+            }
+
+            let output_path = path.parent()
+                .map(|parent| parent.join(&recipe.name))
+                .unwrap_or_else(|| PathBuf::from(&recipe.name));
+
+            println!("Building the model...");
+
+            let model = Model16::build(recipe)?;
+
+            std::fs::write(&output_path, model.into_bytes())?;
+
+            println!("Model saved as {:?}", output_path);
         }
 
         Some("help") | None => {
