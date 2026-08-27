77facd0dd1c3d66750081943f843361417e953bd
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-08-27 14:14:07 +0200

    build: make static musl builds

--- flake.nix
+++ flake.nix
@@ -18,48 +18,27 @@
                     overlays = [ rust-overlay.overlays.default ];
                 };
 
-                config = pkgs.lib.importTOML ./Cargo.toml;
+                muslPkgs = pkgs.pkgsCross.musl64;
 
+                config = pkgs.lib.importTOML ./Cargo.toml;
             in {
-                packages.default = pkgs.rustPlatform.buildRustPackage {
+                packages.default = muslPkgs.rustPlatform.buildRustPackage {
                     pname = config.package.name;
                     version = config.package.version;
 
                     src = ./.;
                     cargoLock.lockFile = ./Cargo.lock;
 
-                    meta = with pkgs.lib; {
-                        description = config.package.description;
-                        homepage = config.package.homepage;
-                        license = licenses.gpl3Only;
-
-                        maintainers = [
-                            {
-                                name = "Nikita Podvirnyi";
-                                email = "krypt0nn@vk.com";
-                                matrix = "@krypt0nn:mozilla.org";
-                                github = "krypt0nn";
-                                githubId = 29639507;
-                            }
-                        ];
-                    };
-
-                    nativeBuildInputs = with pkgs; [
-                        rust-bin.stable.latest.minimal
-                        gcc
-                    ];
+                    doCheck = false;
                 };
 
                 devShells.default = pkgs.mkShell {
                     nativeBuildInputs = with pkgs; [
-                        (rust-bin.stable.latest.default.override {
+                        (pkgs.pkgs.rust-bin.stable.latest.default.override {
+                            targets = [ "x86_64-unknown-linux-musl" ];
                             extensions = [ "rust-src" ];
                         })
-
-                        gcc
-                        cmake
-                        glib
-                        pkg-config
+                        muslPkgs.buildPackages.gcc
                     ];
                 };
             });
